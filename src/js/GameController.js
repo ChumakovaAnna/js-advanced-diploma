98b3b241a9070b84b99abd02384f4189dc62338d
Anna Chumakova <chumakova3008@gmail.com>
2020-06-02 23:27:04 +0500

    Added drawing the gameboard

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,23 +1,26 @@
+import GamePlay from "./GamePlay";
+
 export default class GameController {
-  constructor(gamePlay, stateService) {
-    this.gamePlay = gamePlay;
-    this.stateService = stateService;
-  }
+    constructor(gamePlay, stateService) {
+        this.gamePlay = gamePlay;
+        this.stateService = stateService;
+    }
 
-  init() {
+    init() {
     // TODO: add event listeners to gamePlay events
+        this.gamePlay.drawUi("prairie");
     // TODO: load saved stated from stateService
-  }
+    }
 
-  onCellClick(index) {
+    onCellClick(index) {
     // TODO: react to click
-  }
+    }
 
-  onCellEnter(index) {
+    onCellEnter(index) {
     // TODO: react to mouse enter
-  }
+    }
 
-  onCellLeave(index) {
+    onCellLeave(index) {
     // TODO: react to mouse leave
-  }
+    }
 }
