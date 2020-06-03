09595714c5fae343454c2faf0e8dbe6dc0bd38a4
Anna Chumakova <chumakova3008@gmail.com>
2020-06-03 23:23:31 +0500

    Changed eslint rule

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,24 +1,24 @@
 export default class GameController {
-    constructor(gamePlay, stateService) {
-        this.gamePlay = gamePlay;
-        this.stateService = stateService;
-    }
+  constructor(gamePlay, stateService) {
+    this.gamePlay = gamePlay;
+    this.stateService = stateService;
+  }
 
-    init() {
-        this.gamePlay.drawUi("prairie");
+  init() {
+    this.gamePlay.drawUi("prairie");
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
-    }
+  }
 
-    onCellClick(index) {
+  onCellClick(index) {
     // TODO: react to click
-    }
+  }
 
-    onCellEnter(index) {
+  onCellEnter(index) {
     // TODO: react to mouse enter
-    }
+  }
 
-    onCellLeave(index) {
+  onCellLeave(index) {
     // TODO: react to mouse leave
-    }
+  }
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -4,30 +4,30 @@
  * @param  {number} boardSize - side length of square field
  */
 export function calcTileType(index, boardSize) {
-    const topLeft = 0;
-    const topRight = boardSize - 1;
-    const bottomLeft = boardSize * (boardSize - 1);
-    const bottomRight = boardSize ** 2 - 1;
+  const topLeft = 0;
+  const topRight = boardSize - 1;
+  const bottomLeft = boardSize * (boardSize - 1);
+  const bottomRight = boardSize ** 2 - 1;
 
-    if (index === topLeft) return "top-left";
-    if (index === topRight) return "top-right";
-    if (index === bottomLeft) return "bottom-left";
-    if (index === bottomRight) return "bottom-right";
-    if (index > topLeft && index < topRight) return "top";
-    if (index % boardSize === 0) return "left";
-    if ((index + 1) % boardSize === 0) return "right";
-    if (index > bottomLeft && index < bottomRight) return "bottom";
-    return "center";
+  if (index === topLeft) return "top-left";
+  if (index === topRight) return "top-right";
+  if (index === bottomLeft) return "bottom-left";
+  if (index === bottomRight) return "bottom-right";
+  if (index > topLeft && index < topRight) return "top";
+  if (index % boardSize === 0) return "left";
+  if ((index + 1) % boardSize === 0) return "right";
+  if (index > bottomLeft && index < bottomRight) return "bottom";
+  return "center";
 }
 
 export function calcHealthLevel(health) {
-    if (health < 15) {
-        return "critical";
-    }
+  if (health < 15) {
+    return "critical";
+  }
 
-    if (health < 50) {
-        return "normal";
-    }
+  if (health < 50) {
+    return "normal";
+  }
 
-    return "high";
+  return "high";
 }
