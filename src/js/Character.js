1d264c87e5500e28e87ade759c51968b6ebca197
Anna Chumakova <chumakova3008@gmail.com>
2020-06-06 00:57:00 +0500

    Change levelUp in Character

--- src/js/Character.js
+++ src/js/Character.js
@@ -8,17 +8,22 @@ export default class Character {
     this.defence = 0;
     this.health = 50;
     this.type = type;
-    // TODO: throw error if user use "new Character()"
+
     if (new.target === Character) {
       throw new Error("Do not use new Character()!");
     }
   }
 
+  /**
+   * Raises the level of a living character by 1 and increases its characteristics
+   */
   levelUp() {
     this.level += 1;
-    this.health += 80;
-    if (this.health > 100) {
-      this.health = 100;
+    if (this.health > 0) {
+      this.health += 80;
+      if (this.health > 100) {
+        this.health = 100;
+      }
     }
     this.attack = Math.max(this.attack, this.attack * (1.8 - this.health / 100));
     this.defence = Math.max(this.defence, this.defence * (1.8 - this.health / 100));
