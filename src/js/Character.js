f7d0f07f9604210bf1974c520dcff30a014cbcb4
Anna Chumakova <chumakova3008@gmail.com>
2020-06-05 19:57:28 +0500

    Added levelUp

--- src/js/Character.js
+++ src/js/Character.js
@@ -13,4 +13,14 @@ export default class Character {
       throw new Error("Do not use new Character()!");
     }
   }
+
+  levelUp() {
+    this.level += 1;
+    this.health += 80;
+    if (this.health > 100) {
+      this.health = 100;
+    }
+    this.attack = Math.max(this.attack, this.attack * (1.8 - this.health / 100));
+    this.defence = Math.max(this.defence, this.defence * (1.8 - this.health / 100));
+  }
 }
